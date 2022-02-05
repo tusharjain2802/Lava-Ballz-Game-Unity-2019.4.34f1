4efb9c723df3218671cac68505ddfddf2a0fc01a
Tushar Jain <72308720+tusharjain2802@users.noreply.github.com>
2022-02-06 02:40:13 +0530

    Uploaded ELC Project -Animation and Gaming

---
+++ Lava Ballz/Assets/Scripts/General/Accelarometer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelarometer : MonoBehaviour
+{
+    public bool isFlat = true;
+    private Rigidbody rigid;
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Vector3 tilt = Input.acceleration*20;
+        if(isFlat)
+            tilt = Quaternion.Euler(90,0,0) * tilt;
+    
+        rigid.AddForce(tilt);
+        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.cyan);
+    }
+}
---
+++ Lava Ballz/Assets/Scripts/General/SceneSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void TwiceBack()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+    public void ThriceBack()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+}
